77f6a70b5443a682aa3aac3aca0e822efb1c6458
Boris Karpov <bkarpov@alliedtesting.com>
2020-11-19 18:17:20 +0200

    Updated methods

--- new_class.java
+++ new_class.java
@@ -1,30 +1,41 @@
-method_101
-    method_102
+method_90
+    method_91  updated
+    method_92
+    method_93
+    method_94  updated
+    method_95  updated
+    method_96
+    method_97  updated
+    method_98  updated
+    method_99  updated
+    method_100
+method_101  updated
+    method_102  updated
     method_103
-    method_104
-    method_105
+    method_104  updated
+    method_105  updated
     method_106
     method_107
-    method_108
+    method_108  updated
     method_109
     method_110
     method_111
-    method_112
+    method_112  updated
     method_113
     method_114
-    method_115
+    method_115  updated
     method_116
     method_117
-    method_118
-    method_119
+    method_118  updated
+    method_119  updated
     method_120
     method_121
-    method_122
-    method_123
+    method_122  updated
+    method_123  updated
     method_124
     method_125
-    method_126
-    method_127
+    method_126  updated
+    method_127  updated
     method_128
     method_129
-    method_130
+    method_130  updated
--- old_class.java
+++ old_class.java
@@ -1,100 +1,89 @@
-method_01
+method_01  updated
     method_02
-    method_03
+    method_03  updated
     method_04
     method_05
-    method_06
+    method_06  updated
     method_07
     method_08
     method_09
-    method_10
-    method_11
+    method_10  updated
+    method_11  updated
     method_12
     method_13
-    method_14
-    method_15
-    method_16
+    method_14  updated
+    method_15  updated
+    method_16  updated
     method_17
     method_18
-    method_19
+    method_19  updated
     method_20
-    method_21
+    method_21  updated
     method_22
     method_23
-    method_24
+    method_24  updated
     method_25
     method_26
     method_27
-    method_28
-    method_29
-    method_30
+    method_28  updated
+    method_29  updated
+    method_30  updated
     method_31
     method_32
     method_33
-    method_34
-    method_35
+    method_34  updated
+    method_35  updated
     method_36
     method_37
-    method_38
-    method_39
+    method_38  updated
+    method_39  updated
     method_40
     method_41
-    method_42
-    method_43
+    method_42  updated
+    method_43  updated
     method_44
-    method_45
-    method_46
+    method_45  updated
+    method_46  updated
     method_47
-    method_48
-    method_49
+    method_48  updated
+    method_49  updated
     method_50
     method_51
-    method_52
-    method_53
+    method_52  updated
+    method_53  updated
     method_54
     method_55
-    method_56
-    method_57
+    method_56  updated
+    method_57  updated
     method_58
     method_59
-    method_60
-    method_61
+    method_60  updated
+    method_61  updated
     method_62
     method_63
-    method_64
-    method_65
+    method_64  updated
+    method_65  updated
     method_66
     method_67
     method_68
-    method_69
-    method_70
-    method_71
+    method_69  updated
+    method_70  updated
+    method_71  updated
     method_72
     method_73
-    method_74
+    method_74  updated
     method_75
-    method_76
+    method_76  updated
     method_77
-    method_78
+    method_78  updated
     method_79
-    method_80
-    method_81
+    method_80  updated
+    method_81  updated
     method_82
     method_83
     method_84
     method_85
-    method_86
+    method_86  updated
     method_87
     method_88
-    method_89
-    method_90
-    method_91
-    method_92
-    method_93
-    method_94
-    method_95
-    method_96
-    method_97
-    method_98
-    method_99
-    method_100
+    method_89  updated
