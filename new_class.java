dfe18356076d5a232c969a78dbe32d292ff69768
Boris Karpov <bkarpov@alliedtesting.com>
2020-11-19 10:12:47 +0200

    Update methods in new_class.java and old_class.java

--- new_class.java
+++ new_class.java
@@ -1,2 +1,5 @@
 method_2
-method_4
+    //comment TODO new method
+method_4 updated
+method_6
+method_7
--- old_class.java
+++ old_class.java
@@ -1,5 +1,7 @@
 method_1
-method_2 modified
+method_2 updated
 method_3
 method_4 modified
+method_4_5
+method_4_6
 method_5
